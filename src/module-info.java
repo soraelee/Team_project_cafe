38fb722130c40ddf112621a6ba8d0c62a2169d44
test123 <a@a.com>
2024-03-11 17:44:23 +0900

    프로젝트 기본 사항

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,11 @@
 module Team_project_coffee {
 	exports cafe.main;
-	
+	exports cafe.login.fxml;
+	exports cafe.login.dto;
+	exports cafe.login.main;
+	exports cafe.login.service;
+	exports cafe.login.dao;
+
 	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.fxml;
