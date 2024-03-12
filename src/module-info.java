9b2a9e4b565007746c7c386c317402d1ed24f2d2
test123 <a@a.com>
2024-03-12 16:03:29 +0900

    모듈 파일 변경

--- src/module-info.java
+++ src/module-info.java
@@ -1,12 +1,19 @@
-module Team_project_coffee {
-	exports cafe.main;
+module Team_project_cafe {
+	exports cafe.login.register.service;
+	exports cafe.main.Service;
 	exports cafe.login.fxml;
-	exports cafe.login.dto;
 	exports cafe.login.main;
-	exports cafe.login.service;
+	exports cafe.login.register.dto;
+	exports cafe.login.url;
 	exports cafe.login.dao;
+	exports cafe.login.register.main;
+	exports cafe.main;
+	exports cafe.login.dto;
+	exports cafe.login.register.fxml;
+	exports cafe.login.service;
+	exports cafe.login.register.dao;
 
-	requires javafx.base;
+	requires java.sql;
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
