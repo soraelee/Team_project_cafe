ecc8bdb5df1ce57d36f0c33367b8754f32bf326b
test123 <a@a.com>
2024-03-12 16:36:04 +0900

    main 변경사항

--- src/cafe/main/MainClass.java
+++ src/cafe/main/MainClass.java
@@ -1,5 +1,9 @@
 package cafe.main;
 
+import java.net.URL;
+
+import cafe.login.fxml.LoginController;
+import cafe.login.url.URLService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,6 +25,28 @@ public class MainClass extends Application{
 		arg0.setScene(scene);
 		arg0.show();
 	}
+	public void viewFx(Parent root) {
+		System.out.println("실행");
+		Stage mainStage = (Stage)root.getScene().getWindow(); 
+
+		try {
+			URL url = new URL(URLService.fxPath + "cafe/main/mainfx.fxml");
+			System.out.println(url);
+
+			FXMLLoader loader = new FXMLLoader(url);
+			root = loader.load();
+			Controller ctrl = loader.getController();
+			ctrl.setRoot(root); //root 전달
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Scene scene = new Scene(root);
+		mainStage.setScene(scene);
+		mainStage.show();
+		
+	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
